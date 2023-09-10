b934b9f662c5fa8039ea92543830d649f867ccaf
charonzero <catyx2292000@gmail.com>
2023-09-11 01:20:56 +0630

    SS

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const bot = new ViberBot({
     authToken: "519aa0d6bde7e344-31aa29a1ff2098fe-3b24637725a8a416",
@@ -11,20 +13,25 @@ const bot = new ViberBot({
     avatar: "https://viber.com/avatar.jpg"
 });
 
+const TextMessage = require('viber-bot').Message.Text;
+
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-    response.send(message);
+    const textMessage = new TextMessage('Thank you for your message!');
+    response.send(textMessage);
 });
+
 
 app.use('/viber', bot.middleware());
 
 const port = process.env.PORT || 8080;
-const webhookUrl = "https://18.141.198.28:8080/viber";
+const webhookUrl = "http://18.141.198.28:8080/viber"; // Use HTTP instead of HTTPS
 
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 
     bot.setWebhook(webhookUrl).catch(error => {
-        console.error('Error setting the webhook:', error);
+        console.error('Error setting the webhook:', JSON.stringify(error, null, 2));
     });
+
 });
